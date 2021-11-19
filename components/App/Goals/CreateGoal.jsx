c081e8c4895360ff73888b616c6dfa64e02ec87f
sandypockets <zac@znacreative.com>
2021-11-19 00:45:42 -0500

    Add state to forms

--- components/App/Goals/CreateGoal.jsx
+++ components/App/Goals/CreateGoal.jsx
@@ -1,11 +1,52 @@
 import Card from "../../Global/Card";
 import CardTitle from "../../Global/CardTitle";
+import TextInput from "../../Global/TextInput";
+import {useState} from "react";
 
 export default function CreateGoal() {
+  const [goalTitle, setGoalTitle] = useState('')
+  const [goalDesc, setGoalDesc] = useState('')
+  const [goalOutcome, setGoalOutcome] = useState('')
+  const [goalDueDate, setGoalDueDate] = useState()
+
   return (
     <Card>
       <CardTitle>Create a goal</CardTitle>
       <p>Create a new goal</p>
+      <TextInput type="text" label="Goal title" value={goalTitle} onChangeHandler={(e) => setGoalTitle(e.target.value)} />
+
+      <div className="my-6">
+        <label htmlFor="description" className="block text-sm font-medium text-gray-700 mx-2">
+          Goal description
+        </label>
+        <div className="mt-1 mx-2">
+        <textarea
+          rows={4}
+          name="description"
+          id="description"
+          className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+          value={goalDesc}
+          onChange={(e) => setGoalDesc(e.target.value)}
+        />
+        </div>
+      </div>
+
+      <div>
+        <label htmlFor="outcomes" className="block text-sm font-medium text-gray-700 mx-2">
+          How will you know that this goal is achieved?
+        </label>
+        <div className="mt-1 mx-2">
+        <textarea
+          rows={4}
+          name="outcomes"
+          id="outcomes"
+          className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+          value={goalOutcome}
+          onChange={(e) => setGoalOutcome(e.target.value)}
+        />
+        </div>
+      </div>
+      <div className="mt-6">Date picker</div>
     </Card>
   )
 }
