3428a05c9a1e7db3db1f44375cde285e2d4beb3f
sandypockets <zac@znacreative.com>
2021-11-18 23:15:25 -0500

    Add settings cards

--- pages/app/settings.jsx
+++ pages/app/settings.jsx
@@ -1,9 +1,19 @@
 import AppLayout from "../../components/App/Layout/AppLayout";
+import CardTitle from "../../components/Global/CardTitle";
+import Card from "../../components/Global/Card";
 
 export default function Settings() {
   return (
     <AppLayout>
-      Settings page
+      <CardTitle>Settings</CardTitle>
+      <Card>
+        <CardTitle>Subscription information</CardTitle>
+        <p>Some subscription information</p>
+      </Card>
+      <Card>
+        <CardTitle>Payment info</CardTitle>
+        <p>Some payment form</p>
+      </Card>
     </AppLayout>
   )
 }
