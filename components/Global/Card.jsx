f01046dbfce21c92f2c658b29211d6bf2f4515ae
sandypockets <zac@znacreative.com>
2021-11-18 22:50:00 -0500

    Replace div with section

--- components/Global/Card.jsx
+++ components/Global/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card({ children }) {
   return (
-    <div className="my-4 p-8 rounded-lg shadow-lg">
+    <section className="my-4 p-8 rounded-lg shadow-lg">
       {children}
-    </div>
+    </section>
   )
 }
