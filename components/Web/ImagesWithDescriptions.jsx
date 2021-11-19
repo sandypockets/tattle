0cb505cb0225c44da64748bd7947e938f7f0da5b
sandypockets <zac@znacreative.com>
2021-11-19 00:08:09 -0500

    Add image to section

--- components/Web/ImagesWithDescriptions.jsx
+++ components/Web/ImagesWithDescriptions.jsx
@@ -33,7 +33,7 @@ export default function ImagesWithDescriptions() {
           <div className="mt-12 sm:mt-16 lg:mt-0">
             <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
-                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                className="w-full rounded-xl lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
                 src="/goals-screenshot.png"
                 alt="Goals page interface"
               />
@@ -74,7 +74,7 @@ export default function ImagesWithDescriptions() {
           <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
             <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
-                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
+                className="w-full rounded-xl lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
                 src="/goals-screenshot.png"
                 alt="Goals page interface"
               />
