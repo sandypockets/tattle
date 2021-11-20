7ee7b812b764cc91f7e891743501086929320032
sandypockets <zac@znacreative.com>
2021-11-20 02:16:23 -0500

    Increase y margin

--- components/Global/Card.jsx
+++ components/Global/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ children }) {
   return (
-    <section className="my-4 p-12 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+    <section className="my-8 p-12 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
       {children}
     </section>
   )
