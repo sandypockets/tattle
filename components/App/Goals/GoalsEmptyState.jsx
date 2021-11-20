d9de230e443ab58148a15c0a74b96b280ed3886d
sandypockets <zac@znacreative.com>
2021-11-20 00:35:45 -0500

    Move Card into component

--- components/App/Goals/GoalsEmptyState.jsx
+++ components/App/Goals/GoalsEmptyState.jsx
@@ -1,16 +1,19 @@
 import { CheckIcon } from "@heroicons/react/solid";
+import Card from "../../Global/Card";
 
 export default function GoalsEmptyState({ setState }) {
   return (
-    <button
-      type="button"
-      className="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      onClick={() => setState('create')}
-    >
-      <div className="mx-auto h-12 w-12 text-gray-400">
-        <CheckIcon />
-      </div>
-      <span className="mt-2 block text-sm font-medium text-gray-900">Add your first goal</span>
-    </button>
+    <Card>
+      <button
+        type="button"
+        className="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        onClick={() => setState('create')}
+      >
+        <div className="mx-auto h-12 w-12 text-gray-400">
+          <CheckIcon />
+        </div>
+        <span className="mt-2 block text-sm font-medium text-gray-900">Add your first goal</span>
+      </button>
+    </Card>
   )
 }
