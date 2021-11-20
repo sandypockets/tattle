9c2329888d6f5175d21ae6941fd11288d68666e9
sandypockets <zac@znacreative.com>
2021-11-20 02:16:14 -0500

    Rename column

--- components/App/Settings/YourBillingHistory.jsx
+++ components/App/Settings/YourBillingHistory.jsx
@@ -1,5 +1,6 @@
 import CardTitle from "../../Global/CardTitle";
 import Card from "../../Global/Card";
+import Button from "../../Global/Button";
 
 const bills = [
   { id: '1', title: 'Tattle Monthly', billDate: 'September 1, 2021', amount: '$3 USD' },
@@ -13,62 +14,64 @@ export default function YourBillingHistory() {
   return (
     <Card>
       <CardTitle>Billing history</CardTitle>
-    <div className="flex flex-col">
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="overflow-hidden sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-gray-50">
-              <tr>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  ID
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Title
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Bill date
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Amount
-                </th>
-                <th scope="col" className="relative px-6 py-3">
-                  <span className="sr-only">Edit</span>
-                </th>
-              </tr>
-              </thead>
-              <tbody>
-              {bills.map((bill, index) => (
-                <tr key={bill.id} className={index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{bill.id}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{bill.title}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{bill.billDate}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{bill.amount}</td>
-                  <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                    <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                      View details
-                    </a>
-                  </td>
+      <div className="flex flex-col">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    ID
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Bill type
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Bill date
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Amount
+                  </th>
+                  <th scope="col" className="relative px-6 py-3">
+                    <span className="sr-only">Edit</span>
+                  </th>
                 </tr>
-              ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                {bills.map((bill, index) => (
+                  <tr key={bill.id} className={index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{bill.id}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{bill.title}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{bill.billDate}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{bill.amount}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                      <div className="max-w-min">
+                        <Button>
+                          View details
+                        </Button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </Card>
   )
 }
