686564c49cba93de31ec5f0ef520c062fee8c7cd
sandypockets <zac@znacreative.com>
2021-11-25 20:53:26 -0500

    Create card for smaller grids

---
+++ components/Global/GridCard.jsx
@@ -0,0 +1,7 @@
+export default function GridCard({ children }) {
+  return (
+    <section className="p-6 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+      {children}
+    </section>
+  )
+}
