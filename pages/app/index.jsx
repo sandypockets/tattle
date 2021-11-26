f0ebe272cb4952ed4b6caef9843d2a4f2ae7aa08
sandypockets <zac@znacreative.com>
2021-11-25 23:57:50 -0500

    Add stats to dash

--- pages/app/index.jsx
+++ pages/app/index.jsx
@@ -4,28 +4,64 @@ import AppLayout from "../../components/App/Layout/AppLayout";
 import Button from "../../components/Global/Button";
 import CardTitle from "../../components/Global/CardTitle";
 import GoalCard from "../../components/App/Dashboard/GoalCard";
+import StatsSection from "../../components/Web/StatsSection";
 import getGoals from "../../helpers/getGoals";
 
 export default function Index() {
   const [goals, setGoals] = useState()
-  const [numberOfGoalsToShow, setNumberOfGoalsToShow] = useState(2)
-
-  async function getUserGoals() {
-    const user = await supabase.auth.user()
-    const id = user['id']
-    getGoals(id, setGoals)
-  }
+  const [numberOfGoalsToShow, setNumberOfGoalsToShow] = useState(3)
+  const [userStats, setUserStats] = useState({
+    'heading': 'Pressuring people to keep their goals since 2021.',
+    'subHeading': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus repellat laudantium.',
+    'statOne': '4',
+    'statOneText': 'Goals created',
+    'statTwo': '2',
+    'statTwoText': 'Completed on time',
+    'statThree': '1',
+    'statThreeText': 'times Tattled on',
+  })
 
   useEffect(() => {
+    async function getUserGoals() {
+      const user = await supabase.auth.user()
+      const id = user['id']
+      getGoals(id, setGoals)
+    }
     getUserGoals()
   }, [])
 
-  goals && goals.sort((a,b) => (a['due_date'] > b['due_date']) ? 1 : ((b['due_date'] > a['due_date']) ? -1 : 0)).reverse()
+  useEffect(() => {
+    goals && setUserStats({
+      'statOne': goals.length,
+      'statOneText': 'Goals created',
+      'statTwo': '2',
+      'statTwoText': 'Goals due this week',
+      'statThree': '1',
+      'statThreeText': 'times Tattled on'
+    })
+  }, [])
+
+  // Sort goals by due_date, then by id
+  goals && goals.sort(
+    function(a, b) {
+      if (a['due_date'] > b['due_date']) {
+        return -1
+      } else if (a['due_date'] < b['due_date']) {
+        return 1
+      }
+      if (a['id'] > b['id']) {
+        return -1
+      } else if (a['id'] < b['id']) {
+        return 1
+      }
+    }
+  ).reverse()
 
   return (
     <AppLayout>
-      <CardTitle>Your goals</CardTitle>
-      <div className="grid grid-cols-2 gap-10">
+      <StatsSection statProps={userStats} showHeadings={false} />
+      <CardTitle>Goals due soon</CardTitle>
+      <div className="grid grid-cols-3 gap-5">
         {goals && goals.map((goal, index) => {
           if (index < numberOfGoalsToShow) {
             return (
@@ -37,7 +73,7 @@ export default function Index() {
         })}
       </div>
       <div className="w-36 mx-auto mt-10">
-        <Button onClickHandler={() => setNumberOfGoalsToShow(numberOfGoalsToShow + 4)}>Show more</Button>
+        <Button onClickHandler={() => setNumberOfGoalsToShow(numberOfGoalsToShow + 3)}>Show more</Button>
       </div>
     </AppLayout>
   )
