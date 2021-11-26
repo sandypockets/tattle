e3b53ce58ead5c78731f8cd9432d82375f4ecf5e
sandypockets <zac@znacreative.com>
2021-11-25 23:57:36 -0500

    Refactor props

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,25 @@ import Layout from '../components/Web/Layout/Layout'
 import StatsSection from "../components/Web/StatsSection";
 import ImagesWithDescriptions from "../components/Web/ImagesWithDescriptions";
 
+
+const statProps = {
+  'heading': 'Pressuring people to keep their goals since 2021.',
+  'subHeading': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus repellat laudantium.',
+  'statOne': '90%',
+  'statOneText': 'goal success rate',
+  'statTwo': '180',
+  'statTwoText': 'goals achieved',
+  'statThree': '40',
+  'statThreeText': 'tattle messages sent',
+}
+
 export default function Index() {
 
   return (
     <>
       <Layout>
         <HeroSection />
-        <StatsSection />
+        <StatsSection statProps={statProps} />
         <ImagesWithDescriptions />
       </Layout>
     </>
