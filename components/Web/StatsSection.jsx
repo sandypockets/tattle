8b7dd545525df31c0ecffbb42f33da8a75253705
sandypockets <zac@znacreative.com>
2021-11-25 23:58:51 -0500

    Refactor for reusability

--- components/Web/StatsSection.jsx
+++ components/Web/StatsSection.jsx
@@ -1,16 +1,20 @@
-export default function StatsSection() {
+export default function StatsSection({ statProps, showHeadings=true }) {
+  const { heading, subHeading, statOne, statOneText, statTwo, statTwoText, statThree, statThreeText } = statProps
+
   return (
-    <div className="pt-12 sm:pt-28">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-            Pressuring people to keep their goals since 2021.
-          </h2>
-          <p className="mt-3 text-xl text-gray-500 sm:mt-4">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus repellat laudantium.
-          </p>
+    <div>
+      {showHeadings && (
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="max-w-4xl mx-auto text-center">
+            <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+              {heading}
+            </h2>
+            <p className="mt-3 text-xl text-gray-500 sm:mt-4">
+              {subHeading}
+            </p>
+          </div>
         </div>
-      </div>
+      )}
       <div className="mt-10 pb-12 sm:pb-16">
         <div className="relative">
           <div className="absolute inset-0 h-1/2" />
@@ -18,16 +22,16 @@ export default function StatsSection() {
             <div className="max-w-4xl mx-auto">
               <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
                 <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
-                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">goal success rate</dt>
-                  <dd className="order-1 text-5xl font-extrabold text-yellow-400">90%</dd>
+                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">{statOneText}</dt>
+                  <dd className="order-1 text-5xl font-extrabold text-yellow-400">{statOne}</dd>
                 </div>
                 <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">goals achieved</dt>
-                  <dd className="order-1 text-5xl font-extrabold text-yellow-400">180</dd>
+                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">{statTwoText}</dt>
+                  <dd className="order-1 text-5xl font-extrabold text-yellow-400">{statTwo}</dd>
                 </div>
                 <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
-                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">tattle messages sent</dt>
-                  <dd className="order-1 text-5xl font-extrabold text-yellow-400">40</dd>
+                  <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">{statThreeText}</dt>
+                  <dd className="order-1 text-5xl font-extrabold text-yellow-400">{statThree}</dd>
                 </div>
               </dl>
             </div>
