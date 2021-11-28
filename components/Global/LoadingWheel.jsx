2b28c50492e3d110916daaa5ea4b9d35a846429c
sandypockets <zac@znacreative.com>
2021-11-28 17:24:13 -0500

    Reverse loading icon spin direction

--- components/Global/LoadingWheel.jsx
+++ components/Global/LoadingWheel.jsx
@@ -2,7 +2,7 @@ import { RefreshIcon } from '@heroicons/react/outline'
 
 export default function LoadingWheel() {
   return (
-    <svg className="animate-spin h-16 w-16 mr-3" viewBox="0 0 24 24">
+    <svg className="animate-reverse-spin h-16 w-16 mr-3" viewBox="0 0 24 24">
       <RefreshIcon />
     </svg>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'reverse-spin': 'reverse-spin 1s linear infinite'
+      },
+      keyframes: {
+        "reverse-spin": {
+          from: {
+            transform: 'rotate(360deg)'
+          },
+        }
+      }
+    },
   },
   variants: {
     extend: {},
