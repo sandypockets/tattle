5d87d918bf919c9c04b2e6fb3fbe8d6d4f4146ed
sandypockets <zac@znacreative.com>
2021-11-28 20:53:45 -0500

    Add loading state

--- pages/app/goals/index.jsx
+++ pages/app/goals/index.jsx
@@ -11,8 +11,11 @@ import GoalsEmptyState from "../../../components/App/Goals/GoalsEmptyState";
 import GoalsTable from "../../../components/App/Goals/GoalsTable";
 import getGoals from "../../../helpers/getGoals";
 import EditGoalSlideover from "../../../components/App/Goals/EditGoalSlideover";
+import LoadingWheelWrapper from "../../../components/Global/LoadingWheelWrapper";
+import LoadingWheel from "../../../components/Global/LoadingWheel";
 
 export default function Index() {
+  const [loading, setLoading] = useState(true)
   const [displayFormType, setDisplayFormType] = useState('empty')
   const [goals, setGoals] = useState()
   const [open, setOpen] = useState(false)
@@ -30,36 +33,49 @@ export default function Index() {
     getUserGoals()
   }, [])
 
+  useEffect(() => {
+    user && goals && setLoading(false)
+  }, [goals])
+
   goals && goals.sort((a,b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0))
 
   return (
     <AppLayout>
-      <div className="flex justify-between">
-        <CardTitle>Goals</CardTitle>
-        <div className="max-w-min">
-          <Button onClickHandler={() => setDisplayFormType('create')}>Create</Button>
-        </div>
-      </div>
-      {!goals && (
-        <Banner>
-          <p className="h-12">Before you can create a goal, you need to{' '}
-            <Link href="/app/contacts">
-              <a className="text-yellow-200">
-                create a contact
-              </a>
-            </Link>
-            .</p>
-        </Banner>
+      {loading && (
+        <LoadingWheelWrapper>
+          <LoadingWheel />
+        </LoadingWheelWrapper>
       )}
-      <Card>
-        <CardTitle>Manage your goals</CardTitle>
-        <p className="my-4">Add your mom, your best friend, or anyone else that will help keep you accountable.</p>
-        <p>After saving a contact, you can assign the contact to any goals you create.</p>
-      </Card>
-      {displayFormType === 'empty' && !goals && <GoalsEmptyState setState={setDisplayFormType} />}
-      {displayFormType === 'create' && <CreateGoal setDisplayFormType={setDisplayFormType} getUserGoals={getUserGoals} />}
-      {goals && <GoalsTable goals={goals} setSelectedGoal={setSelectedGoal} setOpen={setOpen} />}
-      <EditGoalSlideover title="Edit goal" open={open} setOpen={setOpen} user={user} selectedGoal={selectedGoal} getUserGoals={getUserGoals} />
+      {!loading && (
+        <>
+          <div className="flex justify-between">
+            <CardTitle>Goals</CardTitle>
+            <div className="max-w-min">
+              <Button onClickHandler={() => setDisplayFormType('create')}>Create</Button>
+            </div>
+          </div>
+          {!goals && (
+            <Banner>
+              <p className="h-12">Before you can create a goal, you need to{' '}
+                <Link href="/app/contacts">
+                  <a className="text-yellow-200">
+                    create a contact
+                  </a>
+                </Link>
+                .</p>
+            </Banner>
+          )}
+          <Card>
+            <CardTitle>Manage your goals</CardTitle>
+            <p className="my-4">Add your mom, your best friend, or anyone else that will help keep you accountable.</p>
+            <p>After saving a contact, you can assign the contact to any goals you create.</p>
+          </Card>
+          {displayFormType === 'empty' && !goals && <GoalsEmptyState setState={setDisplayFormType} />}
+          {displayFormType === 'create' && <CreateGoal setDisplayFormType={setDisplayFormType} getUserGoals={getUserGoals} />}
+          {goals && <GoalsTable goals={goals} setSelectedGoal={setSelectedGoal} setOpen={setOpen} />}
+          <EditGoalSlideover title="Edit goal" open={open} setOpen={setOpen} user={user} selectedGoal={selectedGoal} getUserGoals={getUserGoals} />
+        </>
+      )}
     </AppLayout>
   )
 }
