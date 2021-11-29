c5f7e143a8324a52561ae76df95dfb4c3698f1f5
sandypockets <zac@znacreative.com>
2021-11-28 23:19:34 -0500

    Improve data structure, use next/link

--- components/Web/Layout/Footer.jsx
+++ components/Web/Layout/Footer.jsx
@@ -1,24 +1,26 @@
-const navigation = {
-  main: [
-    { name: 'About', href: '/' },
-    { name: 'Blog', href: '/' },
-    { name: 'Pricing', href: '/' },
-    { name: 'Reports', href: '/' },
-    { name: 'Projects', href: '/' },
-    { name: 'Contact', href: '/' },
-  ]
-}
+import Link from 'next/link'
+
+const navigation = [
+  { name: 'About', href: '/about' },
+  { name: 'FAQ', href: '/faq' },
+  { name: 'How it works', href: '/how-it-works' },
+  { name: 'Pricing', href: '/pricing' },
+  { name: 'Login', href: '/signin' },
+  { name: 'Sign up', href: '/signup' },
+]
 
 export default function Footer() {
   return (
     <footer className="bg-white mt-12">
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
         <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
-          {navigation.main.map((item) => (
+          {navigation.map((item) => (
             <div key={item.name} className="px-5 py-2">
-              <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
-                {item.name}
-              </a>
+              <Link href={item.href}>
+                <a className="text-base text-gray-500 hover:text-gray-900">
+                  {item.name}
+                </a>
+              </Link>
             </div>
           ))}
         </nav>
--- components/Web/Layout/Navigation.jsx
+++ components/Web/Layout/Navigation.jsx
@@ -65,60 +65,60 @@ export default function Navigation({ currentUrl }) {
                 </div>
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                   {/* Profile dropdown */}
-                  <Menu as="div" className="ml-3 relative">
-                    <div>
-                      <Menu.Button className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-400">
-                        <span className="sr-only">Open user menu</span>
-                        <img
-                          className="h-8 w-8 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-200"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <Menu.Item>
-                          {({ active }) => (
-                            <Link href={"/account"}>
-                              <a className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
-                                Your Profile
-                              </a>
-                            </Link>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                            >
-                              Settings
-                            </a>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                              onClick={() => supabase.auth.signOut()}
-                            >
-                              Sign out
-                            </a>
-                          )}
-                        </Menu.Item>
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
+                  {/*<Menu as="div" className="ml-3 relative">*/}
+                  {/*  <div>*/}
+                  {/*    <Menu.Button className="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-400">*/}
+                  {/*      <span className="sr-only">Open user menu</span>*/}
+                  {/*      <img*/}
+                  {/*        className="h-8 w-8 rounded-full"*/}
+                  {/*        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"*/}
+                  {/*        alt=""*/}
+                  {/*      />*/}
+                  {/*    </Menu.Button>*/}
+                  {/*  </div>*/}
+                  {/*  <Transition*/}
+                  {/*    as={Fragment}*/}
+                  {/*    enter="transition ease-out duration-200"*/}
+                  {/*    enterFrom="transform opacity-0 scale-95"*/}
+                  {/*    enterTo="transform opacity-100 scale-100"*/}
+                  {/*    leave="transition ease-in duration-75"*/}
+                  {/*    leaveFrom="transform opacity-100 scale-100"*/}
+                  {/*    leaveTo="transform opacity-0 scale-95"*/}
+                  {/*  >*/}
+                  {/*    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">*/}
+                  {/*      <Menu.Item>*/}
+                  {/*        {({ active }) => (*/}
+                  {/*          <Link href={"/account"}>*/}
+                  {/*            <a className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>*/}
+                  {/*              Your Profile*/}
+                  {/*            </a>*/}
+                  {/*          </Link>*/}
+                  {/*        )}*/}
+                  {/*      </Menu.Item>*/}
+                  {/*      <Menu.Item>*/}
+                  {/*        {({ active }) => (*/}
+                  {/*          <a*/}
+                  {/*            href="#"*/}
+                  {/*            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}*/}
+                  {/*          >*/}
+                  {/*            Settings*/}
+                  {/*          </a>*/}
+                  {/*        )}*/}
+                  {/*      </Menu.Item>*/}
+                  {/*      <Menu.Item>*/}
+                  {/*        {({ active }) => (*/}
+                  {/*          <a*/}
+                  {/*            href="#"*/}
+                  {/*            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}*/}
+                  {/*            onClick={() => supabase.auth.signOut()}*/}
+                  {/*          >*/}
+                  {/*            Sign out*/}
+                  {/*          </a>*/}
+                  {/*        )}*/}
+                  {/*      </Menu.Item>*/}
+                  {/*    </Menu.Items>*/}
+                  {/*  </Transition>*/}
+                  {/*</Menu>*/}
                 </div>
               </div>
             </div>
