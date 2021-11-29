c0ef1811dea65fdb5722f6d76747134cede72a69
sandypockets <zac@znacreative.com>
2021-11-28 21:15:11 -0500

    Improve time left/overdue stat

--- pages/app/goals/[id].jsx
+++ pages/app/goals/[id].jsx
@@ -2,26 +2,25 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { supabase } from "../../../lib/supabaseClient";
 import AppLayout from "../../../components/App/Layout/AppLayout";
+import Button from "../../../components/Global/Button";
 import Card from "../../../components/Global/Card";
 import CardTitle from "../../../components/Global/CardTitle";
 import GridCard from "../../../components/Global/GridCard";
 import LoadingWheel from "../../../components/Global/LoadingWheel";
 import LoadingWheelWrapper from "../../../components/Global/LoadingWheelWrapper";
 import getGoal from "../../../helpers/getGoal";
-import Button from "../../../components/Global/Button";
 import getContact from "../../../helpers/getContact";
 
 export default function SingleGoal() {
-  const [goal, setGoal] = useState()
   const [contact, setContact] = useState()
-  const [timeLeft, setTimeLeft] = useState()
+  const [goal, setGoal] = useState()
   const [loading, setLoading] = useState(true)
+  const [timeLeft, setTimeLeft] = useState()
   const router = useRouter()
   const user = supabase.auth.user()
 
   useEffect(() => {
     const { id } = router.query
-    console.log("goalId", id)
     getGoal(user.id, id, setGoal)
   }, [])
 
@@ -66,8 +65,8 @@ export default function SingleGoal() {
           </section>
           <div className="grid grid-cols-4 gap-6">
             <GridCard>
-              <h2>Time remaining</h2>
-              <CardTitle>{timeLeft}{timeLeft === 1 ? " day" : " days"}</CardTitle>
+              <h2>{timeLeft.toString()[0] === '-' ? "Days late" : "Time remaining"}</h2>
+              <CardTitle>{timeLeft.toString()[0] === '-' ? timeLeft.toString().slice(1) : timeLeft}{timeLeft === 1 ? " day" : " days"}</CardTitle>
             </GridCard>
             <GridCard>
               <h2>Created on</h2>
@@ -82,7 +81,6 @@ export default function SingleGoal() {
               <CardTitle>{contact && contact.name}</CardTitle>
             </GridCard>
           </div>
-
           <Card>
             <CardTitle>Description</CardTitle>
             <p>Description: <span>{goal['description']}</span></p>
