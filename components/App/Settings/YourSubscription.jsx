bdc6d812bec6ca7c181bc851d38ab49fa322c7d2
sandypockets <zac@znacreative.com>
2021-11-30 22:47:29 -0500

    Delay bug fix

--- components/App/Settings/YourSubscription.jsx
+++ components/App/Settings/YourSubscription.jsx
@@ -26,9 +26,11 @@ export default function YourSubscription() {
     <div className="my-6 max-w-lg mx-auto rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden lg:max-w-none lg:flex">
       <div className="flex-1 bg-white px-6 py-8 lg:p-12">
         <h3 className="text-2xl font-extrabold text-gray-900 sm:text-3xl">Subscription</h3>
+      {subscriptionData && (
         <p className="mt-6 text-base text-gray-500">
           You're currently on Tattle's <strong>{subscriptionData['plan_billing_frequency']}</strong> plan, at <strong>${subscriptionData['plan_amount_cents'] / 1000} {subscriptionData['plan_billing_frequency'] === 'monthly' ? 'per month' : 'annually'}.</strong>
         </p>
+      )}
         <div className="mt-8">
           <div className="flex items-center">
             <h4 className="flex-shrink-0 pr-4 bg-white text-sm tracking-wider font-bold uppercase text-yellow-400">
@@ -49,10 +51,12 @@ export default function YourSubscription() {
         </div>
       </div>
       <div className="py-8 px-6 text-center bg-gray-50 lg:flex-shrink-0 lg:flex lg:flex-col lg:justify-center lg:p-12">
-        <div className="mt-4 flex items-center justify-center text-5xl font-extrabold text-gray-900">
-          <span>$3 USD</span>
-          <span className="ml-3 text-xl font-medium text-gray-500"> / month</span>
-        </div>
+        {subscriptionData && (
+          <div className="mt-4 flex items-center justify-center text-5xl font-extrabold text-gray-900">
+            <span>${subscriptionData['plan_amount_cents'] / 1000} USD</span>
+            <span className="ml-3 text-xl font-medium text-gray-500"> / {subscriptionData['plan_billing_frequency'] === 'monthly' ? 'month' : 'year'}</span>
+          </div>
+        )}
 
 
 
