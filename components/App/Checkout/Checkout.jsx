a591db842f7e45c4c18018237a0760e248cd77ef
sandypockets <zac@znacreative.com>
2021-12-03 23:15:34 -0500

    Fix publishable key

--- components/App/Checkout/Checkout.jsx
+++ components/App/Checkout/Checkout.jsx
@@ -7,28 +7,35 @@ import CheckoutForm from "./CheckoutForm";
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
 // loadStripe is initialized with a fake API key.
-const stripeApiKey = process.env.NEXT_STRIPE_SECRET_KEY
-const stripePromise = loadStripe(stripeApiKey);
+const stripePromise = loadStripe('pk_test_51IkyECLSQuRsBVHwF7qm2tCexmpVUdG2fMphLozNAwUelsH4aklQqXVOu8HjkJjq0dWcALrjPfnAQerGxlEpQI8000E8OwzIHi');
 
-export default function Checkout() {
+
+export default function Checkout({ session }) {
   const [clientSecret, setClientSecret] = useState("");
+  const [user, setUser] = useState()
 
   useEffect(() => {
     const user = supabase.auth.user()
-    // Create PaymentIntent as soon as the page loads
-    fetch("/api/v1/stripe2", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        items: [{ id: "Tattle Monthly" }],
-        user: user.id
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setClientSecret(data.clientSecret)
-      });
-  }, []);
+    setUser(user)
+  }, [])
+
+  useEffect(() => {
+    if (session || user) {
+      // Create PaymentIntent as soon as the page loads
+      fetch("/api/v1/stripe2", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          items: [{ id: "Tattle Monthly" }],
+          user: session.user.id
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setClientSecret(data.clientSecret)
+        });
+    }
+  }, [user, session]);
 
   const appearance = {
     theme: 'stripe',
