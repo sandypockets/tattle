76e5582799bab6abc2ed7e9fd7edcba5a914d672
sandypockets <zac@znacreative.com>
2021-12-04 16:29:23 -0500

    Redirect to /app/ after payment

--- components/App/Checkout/CheckoutForm.jsx
+++ components/App/Checkout/CheckoutForm.jsx
@@ -50,7 +50,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000",
+        return_url: "http://localhost:3000/app",
       },
     });
     // This point will only be reached if there is an immediate error when
