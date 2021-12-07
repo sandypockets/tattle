04fabb3e30b2fc8d979ae2d2c13a7ae62f32834c
sandypockets <zac@znacreative.com>
2021-12-07 16:07:02 -0500

    Create dedicated new contact page

--- pages/app/contacts/index.jsx
+++ pages/app/contacts/index.jsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../../lib/supabaseClient";
-import AppLayout from "../../components/App/Layout/AppLayout";
-import ContactsEmptyState from "../../components/App/Contacts/ContactsEmptyState";
-import CreateContact from "../../components/App/Contacts/CreateContact";
-import ContactsTable from "../../components/App/Contacts/ContactsTable";
-import EditContactSlideover from "../../components/App/Contacts/EditContactSlideover";
-import Heading from "../../components/App/Contacts/Heading";
-import IntroCard from "../../components/App/Contacts/IntroCard";
-import LoadingWheel from "../../components/Global/LoadingWheel";
-import LoadingWheelWrapper from "../../components/Global/LoadingWheelWrapper";
-import getContacts from "../../helpers/getContacts";
+import { supabase } from "../../../lib/supabaseClient";
+import AppLayout from "../../../components/App/Layout/AppLayout";
+import ContactsEmptyState from "../../../components/App/Contacts/ContactsEmptyState";
+import CreateContact from "../../../components/App/Contacts/CreateContact";
+import ContactsTable from "../../../components/App/Contacts/ContactsTable";
+import EditContactSlideover from "../../../components/App/Contacts/EditContactSlideover";
+import Heading from "../../../components/App/Contacts/Heading";
+import IntroCard from "../../../components/App/Contacts/IntroCard";
+import LoadingWheel from "../../../components/Global/LoadingWheel";
+import LoadingWheelWrapper from "../../../components/Global/LoadingWheelWrapper";
+import getContacts from "../../../helpers/getContacts";
 
-export default function Contacts() {
+export default function Index() {
   const [loading, setLoading] = useState(true)
   const [displayFormType, setDisplayFormType] = useState('empty')
   const [contacts, setContacts] = useState()
---
+++ pages/app/contacts/new.jsx
@@ -0,0 +1,10 @@
+import AppLayout from "../../../components/App/Layout/AppLayout";
+import CreateContact from "../../../components/App/Contacts/CreateContact";
+
+export default function NewContact() {
+  return (
+    <AppLayout>
+      <CreateContact />
+    </AppLayout>
+  )
+}
