e9ecf5e35d3e468a125cae0ec77d95563f977c0e
sandypockets <zac@znacreative.com>
2021-12-09 01:25:40 -0500

    Add styles

--- components/Global/Button.jsx
+++ components/Global/Button.jsx
@@ -2,7 +2,7 @@ export default function Button({ type, onClickHandler, disabled=false, children 
   return (
     <button
       type={type}
-      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-bold text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-bold text-white bg-yellow-400 hover:bg-yellow-500 transition duration-150 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
       onClick={onClickHandler}
       disabled={disabled}
     >
--- components/Global/Card.jsx
+++ components/Global/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ children }) {
   return (
-    <section className="my-8 p-12 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white">
+    <section className="my-8 p-12 rounded-2xl shadow-lg ring-1 ring-black ring-opacity-5 bg-white">
       {children}
     </section>
   )
