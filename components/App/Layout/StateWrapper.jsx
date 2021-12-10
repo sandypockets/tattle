e94939b47a435157c075fdba9ded71fc231e1710
sandypockets <zac@znacreative.com>
2021-12-10 13:20:22 -0500

    Style checkout

--- components/App/Layout/StateWrapper.jsx
+++ components/App/Layout/StateWrapper.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../../../lib/supabaseClient";
-import CardTitle from "../../Global/CardTitle";
 import Checkout from "../Checkout/Checkout";
 import getUserPlan from "../../../helpers/subscription/getUserPlan";
 
@@ -34,14 +33,26 @@ export default function StateWrapper({ children }) {
   if (hasSubscription === false && !loading) {
     return (
       <>
-        <CardTitle>Checkout</CardTitle>
-        <div className="grid grid-cols-2">
-          <div>
-            <h3>Some title</h3>
-            <p>Some summary</p>
-            <div>$3 USD / month</div>
-            <div>Terms and conditions</div>
-            <div>Powered by Stripe</div>
+        <div className="grid grid-cols-2 mt-16">
+          <div className="mt-12 max-w-sm">
+            <h1 className="text-4xl font-extrabold text-gray-900 mb-6">Tattle monthly</h1>
+            <h4 className="-mt-6 text-md">$3 USD / month. Cancel anytime.</h4>
+            <div className="my-6 grid grid-cols-2 max-w-1xs bg-gray-100 p-4">
+              <div>
+                <p className="font-thin">Subtotal:</p>
+                <p className="font-thin">Taxes:</p>
+                <p className="font-medium text-xl mt-2">Total:</p>
+              </div>
+              <div className="font-thin">
+                <p className="font-thin text-right">$3.00</p>
+                <p className="text-right">$0.00</p>
+                <p className="text-xl font-medium text-right mt-2">$3.00</p>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <small>Terms and conditions</small>
+              <small>Powered by Stripe</small>
+            </div>
           </div>
           <div>
             <Checkout session={session} />
