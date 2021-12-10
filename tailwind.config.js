277e56ec8ea749de75e3d9b6b205ee6f9a73f891
sandypockets <zac@znacreative.com>
2021-12-10 11:30:11 -0500

    Fix/Tailwind CSS grid-cols-[num] is purged on build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    enabled: true,
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    safelist: [
+      'grid-cols-1',
+      'grid-cols-2',
+      'grid-cols-3',
+      'grid-cols-4',
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     maxWidth: {
