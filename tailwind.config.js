2a6b750cdefe0f92a630cbb1b8d9adb323556145
sandypockets <zac@znacreative.com>
2021-12-10 10:53:38 -0500

    Use custom max-w

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,27 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    maxWidth: {
+      '5xs': '6rem',
+      '4xs': '8rem',
+      '3xs': '10rem',
+      '2xs': '12rem',
+      'xs': '20rem',
+      'sm': '24rem',
+      'md': '28rem',
+      'lg': '32rem',
+      'xl': '36rem',
+      '2xl': '42rem',
+      '3xl': '48rem',
+      '4xl': '56rem',
+      '5xl': '64rem',
+      '6xl': '72rem',
+      '7xl': '80rem',
+      'min': 'min-content',
+      'fit': 'fit-content',
+      'full': '100%',
+      '1/2': '50%'
+    },
     extend: {
       animation: {
         'reverse-spin': 'reverse-spin 1s linear infinite'
