909b7127e8e33ec8da43d779252c958532fb2155
sandypockets <zac@znacreative.com>
2021-12-12 21:18:11 -0500

    Add breakpoints

--- components/App/Goals/ChooseContact.jsx
+++ components/App/Goals/ChooseContact.jsx
@@ -26,16 +26,18 @@ export default function ChooseContact({ selectedContactId, setSelectedContactId 
 
 
   return (
-    <div className="flex mx-2 mt-6 justify-between flex-row-reverse">
-      <div className="flex flex-col">
-        <p>Choose a contact</p>
-        <select defaultValue={selectedContactId} className="w-56" onChange={(e) => setSelectedContactId(event.target.value)}>
-          {contacts && contacts.map((option) => (
-            <option value={option.id} key={option.id}>{option.name} - {option.phone}</option>
-          ) )}
-        </select>
+    <div className="flex mx-2 mt-6 justify-between sm:justify-center flex-row-reverse">
+      <div className="flex flex-col flex-col-reverse sm:flex-row-reverse">
+        <div className="flex flex-col mx-2 sm:mx-0">
+          <label className="mt-3 sm:mt-0"><small>Choose a contact</small></label>
+          <select defaultValue={selectedContactId} className="w-full sm:w-56 focus:ring-yellow-400 focus:border-yellow-400 focus:outline-none border-gray-300 rounded-md" onChange={(e) => setSelectedContactId(event.target.value)}>
+            {contacts?.map((option) => (
+              <option value={option.id} key={option.id}>{option.name} - {option.phone}</option>
+            ) )}
+          </select>
+        </div>
+        <p className="self-center mt-4 w-full sm:max-w-md">Select the person you'd like us to contact if you don't achieve your goal.</p>
       </div>
-      <p className="self-center mt-4 max-w-md">Select the person you'd like us to contact if you don't achieve your goal.</p>
     </div>
   )
 }
--- components/App/Goals/CreateGoal.jsx
+++ components/App/Goals/CreateGoal.jsx
@@ -29,8 +29,10 @@ export default function CreateGoal({ getUserGoals, setDisplayFormType }) {
 
   return (
     <Card>
-      <TextInput type="text" label="Goal title" value={goalTitle} onChangeHandler={(e) => setGoalTitle(e.target.value)} />
-      <div className="my-6">
+      <div className="mx-2">
+        <TextInput type="text" label="Goal title" value={goalTitle} onChangeHandler={(e) => setGoalTitle(e.target.value)} />
+      </div>
+      <div className="my-6 mx-2">
         <label htmlFor="description" className="block text-sm font-medium text-gray-700 mx-2">
           Goal description
         </label>
@@ -39,13 +41,13 @@ export default function CreateGoal({ getUserGoals, setDisplayFormType }) {
           rows={4}
           name="description"
           id="description"
-          className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+          className="shadow-sm focus:ring-yellow-400 focus:border-yellow-400 focus:outline-none block w-full sm:text-sm border-gray-300 rounded-md"
           value={goalDesc}
           onChange={(e) => setGoalDesc(e.target.value)}
         />
         </div>
       </div>
-      <div>
+      <div className="mx-2">
         <label htmlFor="outcomes" className="block text-sm font-medium text-gray-700 mx-2">
           How will you know that this goal is achieved?
         </label>
@@ -54,23 +56,31 @@ export default function CreateGoal({ getUserGoals, setDisplayFormType }) {
           rows={4}
           name="outcomes"
           id="outcomes"
-          className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+          className="shadow-sm focus:ring-yellow-400 focus:border-yellow-400 focus:outline-none block w-full sm:text-sm border-gray-300 rounded-md"
           value={goalOutcome}
           onChange={(e) => setGoalOutcome(e.target.value)}
         />
         </div>
       </div>
       <ChooseContact selectedContactId={selectedContactId} setSelectedContactId={setSelectedContactId} />
-      <div className="flex justify-between flex-row-reverse my-6 mx-2">
-        <div>
-          <p className="mb-1">
-            Due date
-          </p>
-          <SingleDatePicker selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
+
+      <div className="flex justify-between sm:justify-center flex-row-reverse my-6 mx-2">
+        <div className="flex flex-col flex-col-reverse sm:flex-row-reverse">
+          <div className="flex flex-row">
+            <div className="mx-2">
+              <label className="mt-5 sm:mt-0 mb-1">
+                <small>
+                  Due date
+                </small>
+              </label>
+              <SingleDatePicker selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
+            </div>
+          </div>
+          <p className="self-center w-full sm:max-w-md mb-2 sm:mb-0 mt-2">If you do not mark your goal as complete by this date, we'll tattle on you.</p>
         </div>
-        <p className="self-center max-w-md mt-2">If you do not mark your goal as complete by this date, we'll tattle on you.</p>
       </div>
-      <div className="mx-2">
+
+      <div className="mx-4 pt-6 pb-8 sm:pb-6">
         <Button
           onClickHandler={() => {
             handleCreateGoal()
