d2638a44ba7c2ada8c752eeab37317636c7048e6
sandypockets <zac@znacreative.com>
2021-12-15 00:11:38 -0500

    Adjust breakpoint

--- components/App/Dashboard/Stats.jsx
+++ components/App/Dashboard/Stats.jsx
@@ -12,7 +12,7 @@ export default function Stats({ statProps }) {
           <div className="absolute inset-0 h-1/2" />
           <div className="relative max-w-7xl mx-auto">
             <div className="max-w-4xl mx-auto">
-              <dl className="rounded-lg bg-white shadow-lg xs:grid xs:grid-cols-2 sm:grid-cols-4">
+              <dl className="rounded-lg bg-white shadow-lg xs:grid xs:grid-cols-2 md:grid-cols-4">
 
                 <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r xs:border-r">
                   <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">{statOneText}</dt>
