58ea6b8b0475918088c534ceb33152bb6560ee44
sandypockets <zac@znacreative.com>
2021-12-14 23:06:22 -0500

    Improve homepage absolute positioning

--- components/Web/ImagesWithDescriptions.jsx
+++ components/Web/ImagesWithDescriptions.jsx
@@ -2,10 +2,10 @@ import { InboxIcon, SparklesIcon } from '@heroicons/react/outline'
 
 export default function ImagesWithDescriptions() {
   return (
-    <div className="relative bg-white pt-16 pb-32 overflow-hidden">
-      <div>
-        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+    <div className="bg-white pt-16 pb-32 overflow-hidden">
+      <div className="pl-16">
+        <div className="lg:mx-auto lg:max-w-none lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+          <div className="px-4 max-w-xl mx-auto lg:py-16 lg:max-w-4xl lg:mx-0 lg:px-0 lg:pl-10">
             <div>
               <div>
                 <span className="h-12 w-12 rounded-md flex items-center justify-center bg-yellow-400">
@@ -30,10 +30,10 @@ export default function ImagesWithDescriptions() {
               </div>
             </div>
           </div>
-          <div className="mt-12 sm:mt-16 lg:mt-0">
-            <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative">
+          <div className="mt-12 sm:mt-16 lg:mt-0 lg:w-320">
+            <div className="pl-4 lg:px-0 lg:m-0 lg:relative">
               <img
-                className="w-full rounded-xl lg:absolute lg:left-0 lg:max-w-none"
+                className="max-w-3xl rounded-xl lg:absolute lg:left-0"
                 src="/goals-screenshot.png"
                 alt="Index page interface"
               />
@@ -41,9 +41,9 @@ export default function ImagesWithDescriptions() {
           </div>
         </div>
       </div>
-      <div className="mt-24">
+      <div className="mt-56">
         <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+          <div className="px-4 max-w-xl mx-auto lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
             <div>
               <div>
                 <span className="h-12 w-12 rounded-md flex items-center justify-center bg-yellow-400">
@@ -71,10 +71,10 @@ export default function ImagesWithDescriptions() {
               </div>
             </div>
           </div>
-          <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
-            <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative">
+          <div className="lg:block mt-12 sm:mt-16 lg:mt-12 w-full lg:w-320">
+            <div className="lg:px-0 lg:m-0 static md:flex md:justify-center lg:block">
               <img
-                className="w-full rounded-xl lg:absolute lg:left-0 lg:max-w-none"
+                className="static max-w-xl md:max-w-2xl lg:max-w-3xl rounded-xl lg:absolute lg:right-200"
                 src="/goals-screenshot.png"
                 alt="Index page interface"
               />
