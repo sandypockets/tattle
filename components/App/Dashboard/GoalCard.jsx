7fbf713586a2a33c7533c75464b785f8b3709c8f
sandypockets <zac@znacreative.com>
2021-12-17 21:59:36 -0500

    Improve styles

--- components/App/Dashboard/GoalCard.jsx
+++ components/App/Dashboard/GoalCard.jsx
@@ -27,27 +27,34 @@ export default function GoalCard({ goal }) {
     }}>
     <GridCard>
       <h2 className="font-extrabold text-gray-900 text-lg truncate">{goal.title}</h2>
-      <div className="border-b-2 border-gray-150 w-full my-2 mr-2" />
+      <div className="border-b-2 border-gray-150 w-full mt-2 mb-3 mr-2" />
       <p className="text-sm h-10 overflow-hidden truncate mb-2 sm:mb-4">
         {goal.description}
       </p>
-      <small>
-        {goal['is_completed'] === false &&
-          <>
-            <span className="text-gray-400">Due by: </span>
-            <span className={isUrgent ? 'text-red-500' : 'text-gray-500'}>{goal['due_date']}</span>
-          </>
-        }
-        {goal['is_completed'] === true &&
-          <div className="flex">
-            <div className="text-gray-400 w-4 h-4 mr-1"><CheckCircleIcon /></div>
-            <span className="text-gray-400 pr-1">
-              Completed
-            </span>
-            {goal['is_completed_on_time'] === false ? <span className="text-red-400">late</span> : <span className="text-green-500">on time</span>}
-          </div>
-        }
-      </small>
+      <div className="flex justify-between pr-1">
+        <small>
+          {goal['is_completed'] === false &&
+            <>
+              <span className="text-gray-400">Due by: </span>
+              <span className={isUrgent ? 'text-red-500' : 'text-gray-500'}>{goal['due_date']}</span>
+            </>
+          }
+          {goal['is_completed'] === true &&
+            <div className="flex">
+              <div className="text-gray-400 w-4 h-4 mr-1"><CheckCircleIcon /></div>
+              <span className="text-gray-400 pr-1">
+                Completed
+              </span>
+              {goal['is_completed_on_time'] === false ? <span className="text-red-400">late</span> : <span className="text-green-500">on time</span>}
+            </div>
+          }
+        </small>
+        {!goal['is_completed_on_time'] && (
+          <small>
+            <span className="text-red-400">Past due</span>
+          </small>
+        )}
+      </div>
     </GridCard>
     </div>
   )
