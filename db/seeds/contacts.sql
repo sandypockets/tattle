5d78574d99dc2b02d9e49165df05ecf35c8e1ec4
sandypockets <zac@znacreative.com>
2022-01-01 22:11:56 -0500

    Add seeds

---
+++ db/seeds/contacts.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.contacts (owner_id, name, phone, country_code)
+VALUES
+    ('your_unique_owner_id', 'Kobe', '1234567890', 'CA'),
+    ('your_unique_owner_id', 'Cooper', '1234567890', 'CA'),
+    ('your_unique_owner_id', 'Webster', '1234567890', 'CA'),
+    ('your_unique_owner_id', 'Stella', '1234567890', 'CA'),
+    ('your_unique_owner_id', 'Wendell', '1234567890', 'CA'),
+    ('your_unique_owner_id', 'Abby', '1234567890', 'CA');
---
+++ db/seeds/goals.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.goals (owner_id, title, description, outcome, contact_id, owner_name)
+VALUES
+    ('your_unique_owner_id', 'First goal title', 'Create some goals', 'Will have some goals', 1, 'your_first_name'),
+    ('your_unique_owner_id', 'Second goal title', 'Create even more goals', 'Then there will be plenty of goals to test with', 1, 'your_first_name');
