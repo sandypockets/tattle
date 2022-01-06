44f283dddaa9fcaca36ca1011ba0fabc2a76ae37
sandypockets <zac@znacreative.com>
2022-01-05 23:31:51 -0500

    Improve hover state

--- components/App/Goals/GoalsEmptyState.jsx
+++ components/App/Goals/GoalsEmptyState.jsx
@@ -8,7 +8,7 @@ export default function GoalsEmptyState({ setState }) {
         type="button"
         className="
                 relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center
-                hover:border-gray-400 dark:hover:border-gray-50 dark:hover:text-gray-50 text-gray-300
+                hover:border-gray-400 hover:text-gray-500 dark:hover:border-gray-50 dark:hover:text-gray-50 dark:text-gray-300 text-gray-300
                 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 duration-150"
         onClick={() => setState('create')}
       >
