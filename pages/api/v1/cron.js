60337147dfa1f949b54daac6a9a4f1aecaf737ee
sandypockets <zac@znacreative.com>
2022-01-15 21:29:30 -0500

    Tidy code

--- pages/api/v1/cron.js
+++ pages/api/v1/cron.js
@@ -2,7 +2,6 @@ import { supabase } from "../../../lib/supabaseClient";
 
 const accountSid = process.env.NEXT_TWILIO_ACCOUNT_SID;
 const authToken = process.env.NEXT_TWILIO_AUTH_TOKEN;
-const toPhoneNumber = process.env.NEXT_TEST_TO_PHONE_NUMBER;
 const fromPhoneNumber = process.env.NEXT_TEST_FROM_PHONE_NUMBER;
 const client = require('twilio')(accountSid, authToken);
 
@@ -12,24 +11,27 @@ async function getCustomMessages(ownerId) {
       .from('custom_messages')
       .select('custom_sms_message, custom_voice_message')
       .match({ owner_id: ownerId })
-    data && console.log("Custom Messages Data: ", data)
-    return data
+    return data || error
   } catch (err) {
     console.error(err)
   }
 }
 
-async function saveTattle(ownerId, goalId, contactId, messageBody, messageType) {
+async function saveTattle(ownerId, goalId, contactId, messageBody, messageType, tattleSid) {
   try {
     const { data, error } = await supabase
       .from('tattles')
       .insert([
-        { owner_id: ownerId, goal_id: goalId, contact_id: contactId, message_body: messageBody, message_type: messageType }
+        {
+          owner_id: ownerId,
+          goal_id: goalId,
+          contact_id: contactId,
+          message_body: messageBody,
+          message_type: messageType,
+          tattle_sid: tattleSid
+        }
       ])
-    if (data) {
-      console.log("SaveTattle: ", data)
-    }
-    error && console.error(error)
+    return data || error
   } catch (err) {
     console.error(err)
   }
@@ -42,16 +44,14 @@ function parseVariables(customMessageText, contactName, userName, goalTitle) {
   return customMessageText.replace(varContactName, contactName).replace(varUserName, userName).replace(varGoalTitle, goalTitle)
 }
 
-async function sendSmsMessage(contactName, goalTitle, ownerName, ownerId, goalId, contactId, contactPhone, notificationType) {
+async function sendTattle(contactName, goalTitle, ownerName, ownerId, goalId, contactId, contactPhone, notificationType) {
   function convertToUsableNumber(phoneNumber) {
     let hasHyphens = true
     while (hasHyphens) {
       if (phoneNumber.search('-') === -1) {
-        console.log("Phone number is ready")
         hasHyphens = false
         return `+1${phoneNumber}`
       } else {
-        console.log('Removing characters...')
         phoneNumber = phoneNumber.replace('-', '')
       }
     }
@@ -66,15 +66,12 @@ async function sendSmsMessage(contactName, goalTitle, ownerName, ownerId, goalId
     const customVoice = customMessages[0].custom_voice_message
     parsedSms = parseVariables(customSms, contactName, ownerName, goalTitle)
     parsedVoice = parseVariables(customVoice, contactName, ownerName, goalTitle)
-    console.log("customSms", parsedSms)
-    console.log("customVoice", parsedVoice)
   } else {
     parsedSms = `Hey ${contactName}! ${ownerName} didn't achieve their goal: ${goalTitle}`;
     parsedVoice = `Hey ${contactName}! ${ownerName} didn't achieve their goal: ${goalTitle}`;
   }
 
   if (notificationType === 'voice') {
-    // Send voice message
     client.calls
       .create({
         twiml: `<Response><Say>${parsedVoice}</Say></Response>`,
@@ -82,13 +79,10 @@ async function sendSmsMessage(contactName, goalTitle, ownerName, ownerId, goalId
         from: fromPhoneNumber
       })
       .then(call => {
-        console.log(call.sid)
-        console.log(ownerId)
-        return saveTattle(ownerId, goalId, contactId, parsedVoice, 'voice')
+        return saveTattle(ownerId, goalId, contactId, parsedVoice, 'voice', call.sid)
       })
       .catch((err) => console.error(err));
   } else {
-    // Send sms
     client.messages
       .create({
         body: parsedSms,
@@ -96,9 +90,7 @@ async function sendSmsMessage(contactName, goalTitle, ownerName, ownerId, goalId
         to: formattedPhoneNumber
       })
       .then(message => {
-        console.log(message.sid)
-        console.log(ownerId)
-        return saveTattle(ownerId, goalId, contactId, parsedSms, 'sms')
+        return saveTattle(ownerId, goalId, contactId, parsedSms, 'sms', message.sid)
       })
       .catch((err) => console.error(err));
   }
@@ -129,12 +121,10 @@ export async function getContactData(contactId, goalId, goalTitle, ownerName, ow
           .select('username')
           .eq('id', ownerId)
         if (data) {
-          console.log("Data! ", data[0])
           outputData = {
             ...outputData,
             "owner_name": data[0]['username']
           }
-          console.log("outputData! ", outputData)
           return outputData;
         }
         if (error) console.error(error)
@@ -147,15 +137,13 @@ export async function getContactData(contactId, goalId, goalTitle, ownerName, ow
     }
   } catch (err) {
     console.error(err)
-  } finally {
-
   }
 }
 
 async function cronCheck(req, res) {
   const todayDate = new Date().toLocaleDateString('en-CA').toString()
   try {
-    const { data, error, status } = await supabase
+    const { data, error } = await supabase
       .from('goals')
       .select('due_date, id, contact_id, title, owner_name, owner_id, notification_type')
       .eq('due_date', todayDate)
@@ -171,9 +159,8 @@ async function cronCheck(req, res) {
           data[item]['notification_type'])
         dataArr.push(contactData)
       }
-      console.log("dataArr: ", dataArr)
       for (const item in dataArr) {
-        sendSmsMessage(
+        await sendTattle(
           dataArr[item]['contact_name'],
           dataArr[item]['goal_title'],
           dataArr[item]['owner_name'],
@@ -196,12 +183,8 @@ async function cronCheck(req, res) {
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    console.log("CRON FIRED!")
-    // res.status(200).json('Cron job fired!')
+    console.log("Cron job fired.")
     return cronCheck(req, res)
-
-  } else if (req.method === 'GET') {
-    //
   } else {
     res.send("Something's not right. Check your query.").end()
   }
